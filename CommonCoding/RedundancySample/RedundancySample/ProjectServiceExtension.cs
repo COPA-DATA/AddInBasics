b7578b8b48c819a761a9c452bf298aef2281aacc
Christian Feitler <Christian.Feitler@copadata.com>
2024-07-05 18:07:02 +0200

    added missing ProjectNetworkTypes for sake of explainability

--- CommonCoding/RedundancySample/RedundancySample/ProjectServiceExtension.cs
+++ CommonCoding/RedundancySample/RedundancySample/ProjectServiceExtension.cs
@@ -7,87 +7,94 @@ using Scada.AddIn.Contracts.Variable;
 
 namespace RedundancySample
 {
-    /// <summary>
-    /// Description of Project Service Extension.
-    /// </summary>
-    [AddInExtension("Redundancy Sample", "Demonstrates the usage of IProject.ProjectNetworkType and corresponding events StandbyToServerChanging and ServerToStandbyChanging")]
-    public class ProjectServiceExtension : IProjectServiceExtension
+  /// <summary>
+  /// Description of Project Service Extension.
+  /// </summary>
+  [AddInExtension("Redundancy Sample", "Demonstrates the usage of IProject.ProjectNetworkType and corresponding events StandbyToServerChanging and ServerToStandbyChanging")]
+  public class ProjectServiceExtension : IProjectServiceExtension
+  {
+    private readonly BackgroundWorker _backgroundWorker;
+    private IProject _context;
+    private IVariable _variable;
+    private bool _runBackgroundWorker;
+    private int _counter;
+
+    public ProjectServiceExtension()
     {
-        private readonly BackgroundWorker _backgroundWorker;
-        private IProject _context;
-        private IVariable _variable;
-        private bool _runBackgroundWorker;
-        private int _counter;
+      _backgroundWorker = new BackgroundWorker();
+      _backgroundWorker.DoWork += _backgroundWorker_DoWork;
+    }
 
-        public ProjectServiceExtension()
+    #region IProjectServiceExtension implementation
+
+    public void Start(IProject context, IBehavior behavior)
+    {
+      _context = context;
+
+      try
+      {
+        _variable = _context.VariableCollection["AddInCounter"];
+
+        context.StandbyToServerChanging += Context_StandbyToServerChanging;
+        context.ServerToStandbyChanging += ContextOnServerToStandbyChanging;
+
+
+        switch (context.ProjectNetworkType)
         {
-            _backgroundWorker = new BackgroundWorker();
-            _backgroundWorker.DoWork += _backgroundWorker_DoWork;
-        }
-
-        #region IProjectServiceExtension implementation
-
-        public void Start(IProject context, IBehavior behavior)
-        {
-            _context = context;
-
-            try
-            {
-                _variable = _context.VariableCollection["AddInCounter"];
-
-                context.StandbyToServerChanging += Context_StandbyToServerChanging;
-                context.ServerToStandbyChanging += ContextOnServerToStandbyChanging;
-
-
-                switch (context.ProjectNetworkType)
-                {
-                    case ProjectNetworkType.Server:
-                    case ProjectNetworkType.Mainstation:
-                        _runBackgroundWorker = true;
-                        _backgroundWorker.RunWorkerAsync();
-                        break;
-                }
-
-                MessageBox.Show("The service ist started in mode: " + context.ProjectNetworkType);
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.Message);
-            }
-        }
-
-        private void _backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            while (_runBackgroundWorker)
-            {
-                if (_counter == Int32.MaxValue)
-                {
-                    _counter = 0;
-                }
-
-                _variable.SetValue(0, _counter++);
-                Thread.Sleep(1000);
-            }
-        }
-
-        private void ContextOnServerToStandbyChanging(object sender, ServerToStandbyChangingEventArgs e)
-        {
-            _runBackgroundWorker = false;
-        }
-
-        private void Context_StandbyToServerChanging(object sender, StandbyToServerChangingEventArgs e)
-        {
+          case ProjectNetworkType.Server:
+          case ProjectNetworkType.Mainstation:
             _runBackgroundWorker = true;
             _backgroundWorker.RunWorkerAsync();
+            break;
+          case ProjectNetworkType.Client:
+          case ProjectNetworkType.StandBy:
+            _runBackgroundWorker = false;
+            break;
+          default:
+            _runBackgroundWorker = false;
+            break;
         }
 
-        public void Stop()
-        {
-            _context.StandbyToServerChanging -= Context_StandbyToServerChanging;
-            _context.ServerToStandbyChanging -= ContextOnServerToStandbyChanging;
-            _context = null;
-        }
-
-        #endregion
+        MessageBox.Show("The service ist started in mode: " + context.ProjectNetworkType);
+      }
+      catch (Exception e)
+      {
+        MessageBox.Show(e.Message);
+      }
     }
+
+    private void _backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+    {
+      while (_runBackgroundWorker)
+      {
+        if (_counter == Int32.MaxValue)
+        {
+          _counter = 0;
+        }
+
+        _variable.SetValue(0, _counter++);
+        Thread.Sleep(1000);
+      }
+    }
+
+    private void ContextOnServerToStandbyChanging(object sender, ServerToStandbyChangingEventArgs e)
+    {
+      _runBackgroundWorker = false;
+    }
+
+    private void Context_StandbyToServerChanging(object sender, StandbyToServerChangingEventArgs e)
+    {
+      _runBackgroundWorker = true;
+      _backgroundWorker.RunWorkerAsync();
+    }
+
+    public void Stop()
+    {
+      _context.StandbyToServerChanging -= Context_StandbyToServerChanging;
+      _context.ServerToStandbyChanging -= ContextOnServerToStandbyChanging;
+      _context = null;
+    }
+
+    #endregion
+  }
 }
